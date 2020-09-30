cd5096a2ffcf65f56dda7619db730141f85b3480
dohaessam <doha_essam@mivors.com>
2020-09-30 02:44:22 +0200

    Updates

---
+++ precache-manifest.6aeb902727368da8430401f5114dc8c9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7516676d43f5ff9492f4878e4959a72",
+    "url": "/Portifolio-/index.html"
+  },
+  {
+    "revision": "c49e5bd5fc255fa78b5a",
+    "url": "/Portifolio-/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cede65ffdc343c24cf83",
+    "url": "/Portifolio-/static/css/main.413d79aa.chunk.css"
+  },
+  {
+    "revision": "c49e5bd5fc255fa78b5a",
+    "url": "/Portifolio-/static/js/2.c779dbd7.chunk.js"
+  },
+  {
+    "revision": "fdb2a94942838aa2523d65a2238ba74d",
+    "url": "/Portifolio-/static/js/2.c779dbd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cede65ffdc343c24cf83",
+    "url": "/Portifolio-/static/js/main.86815b96.chunk.js"
+  },
+  {
+    "revision": "a56c95012537bfcbe0e0",
+    "url": "/Portifolio-/static/js/runtime-main.5aec84de.js"
+  },
+  {
+    "revision": "5c58e1a23accebe5e9db738f81306211",
+    "url": "/Portifolio-/static/media/bootstrap.5c58e1a2.png"
+  },
+  {
+    "revision": "88580b9464c34b83eab864a708b3c62b",
+    "url": "/Portifolio-/static/media/css.88580b94.png"
+  },
+  {
+    "revision": "b873c3c19d7ef0496767f543f57964c1",
+    "url": "/Portifolio-/static/media/jquery.b873c3c1.png"
+  },
+  {
+    "revision": "77a0f888b9e9a25baf62887465d0daae",
+    "url": "/Portifolio-/static/media/laravel.77a0f888.png"
+  },
+  {
+    "revision": "bab2c760c60f17191cb3a002e08a3dbf",
+    "url": "/Portifolio-/static/media/mysql.bab2c760.png"
+  },
+  {
+    "revision": "bf21386e2dea081c7549a9c481b4503d",
+    "url": "/Portifolio-/static/media/nestjs.bf21386e.png"
+  },
+  {
+    "revision": "0b311c4249bf7f5c31d23242a726af0b",
+    "url": "/Portifolio-/static/media/nodejs.0b311c42.png"
+  },
+  {
+    "revision": "8ef414b9525833b3d925e9ae07ad9feb",
+    "url": "/Portifolio-/static/media/php.8ef414b9.png"
+  },
+  {
+    "revision": "1f8fb15c99ab617d3fea6bba744fa9cc",
+    "url": "/Portifolio-/static/media/postgress.1f8fb15c.png"
+  },
+  {
+    "revision": "f2de08a9f0f0e488d11c881d65acd20f",
+    "url": "/Portifolio-/static/media/pro2.f2de08a9.jpg"
+  },
+  {
+    "revision": "76fc9aa252b70c510358bf5b9b4c1abc",
+    "url": "/Portifolio-/static/media/python.76fc9aa2.png"
+  },
+  {
+    "revision": "b5e2b8ca34577c9e4805abeb42e781ea",
+    "url": "/Portifolio-/static/media/react.b5e2b8ca.png"
+  }
+]);
